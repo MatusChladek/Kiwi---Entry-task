208e7a48100b31323b9dbb76f96a3696b0ec34eb
Matus Chladek <mmchladek@gmail.com>
2018-08-27 01:11:58 +0200

    Draft

--- find_combinations.py
+++ find_combinations.py
@@ -13,68 +13,72 @@ def get_flights(df,bags):
     output = {}
 
     for start in locs:
-        #print(start)
         flights = []
 
         # create list of all flights from start destination
-        # consider flight_number to be unique index temporarily
+        # consider inices to not be flight_number since there
+        # can be repeating flight_numbers in different days
         df_start = df.loc[df.source == start,:]
-        #print(df_start)
         df_start = df_start.loc[df_start.bags_allowed >= bags,:]
+
         for item in df_start.index:
             flights.append([item])
 
         flights = np.array(flights)
-        #print(flights)
 
         # longest trips from start destination for each flight
         new = create_flights(df,flights,bags)
-        #print(new)
 
-        # create subsets of longest trips
+        # create all valid subsets of longest trips
         subsets = get_subsets(new)
-        #print(subsets)
 
-        # delete those with repeating locations
+        # delete those trips with repeating locations (exept for start/end)
         indexes = del_repeats(df,subsets,1)
         uniques = [subsets[i] for i in indexes]
+
+        # delete repeating trips
         uniques_set = set(tuple(x) for x in uniques)
         uniques = [list(x) for x in uniques_set]
-        uniques
-        #print(uniques)
 
         # add price to the end of each trip
         final = add_price(df,uniques,bags)
 
-        # map it to create proper trip (I think that storing just ID is best way for further processing)
+        # map IDs to df rows for postproccessing
+        # though I think that storing just ID is best way for further processing
         final = map_index(df,final)
 
         output[start] = final
     return output
 
-# in case we want to do it directly from file
-#df = pd.read_csv('data1.csv',parse_dates=True)
+# 1) in case we want to do it directly from file
+# df = pd.read_csv('data1.csv',parse_dates=True)
 
-# read console
+# 2) read console
 import csv
 import sys
 
 f = sys.stdin.read().splitlines()
-lines = csv.reader(f, delimiter=';')
+lines = csv.reader(f)
 lines = list(lines)
 column_names = lines[0]
 rows = lines[1:]
 df = pd.DataFrame(columns=column_names)
 for i in range(len(rows)):
     df.loc[i] = rows[i]
+df.price = df.price.astype(int)
+df.bags_allowed = df.bags_allowed.astype(int)
+df.bags_price = df.bags_price.astype(int)
 
-print(df)
 
 
-
+# print all combinations
+# of flights for passengers with no bags, one bag or two bags
 bags_0 = get_flights(df,0)
 print("All routes for passengers without a bag: ",bags_0)
 bags_1 = get_flights(df,1)
 print("All routes for passengers with one bag: ",bags_1)
 bags_2 = get_flights(df,2)
-print("All routes for passengers with two bags: ",bags_2)
+print("All routes for passengers with two bags: ", bags_2)
+
+# Output is dictionary with locations and nested lists where
+# price is always last item
--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
 def create_flights(df,flights,bags):
     output = []
     count_check = 0
-
     df1 = df.loc[df.bags_allowed >= bags,:]
-    # set proper unique identifier here brah
-    #df1 = df1.set_index('flight_number')
 
     for counter,route in enumerate(flights):
-        #print(route)
+        # take just last flight
         pls = df1.loc[route[-1],:]
+
         # filter just rows with correct source
         temp_df = df1.loc[df1.source == pls.destination,:]
+
         # filter just time wise available fligths
+        # using more/less or equal based on my understanding
+        # above actually makes difference
         temp_df['wait'] = (temp_df.departure - pls.arrival)/pd.Timedelta('1h')
         temp_df = temp_df.query('1<=wait<=4')
 
-        if len(temp_df)!=0:
+        if len(temp_df) != 0:
             #checking if there are any new flights at all
             count_check += 1
-
             for item in temp_df.index:
                 new_route = np.append(route,item)
                 output.append(new_route)
@@ -26,47 +26,45 @@ def create_flights(df,flights,bags):
             output.append(route)
     #print(count_check)
 
+    # do until no new flights were found for all current trips
     if count_check != 0 :
         output = create_flights(df,output,bags)
 
     return output
 
-# create all subsets with initial start
+#####################################################################
+
+# create all subsets of longest trips starting with initial start
 def get_subsets(route_list):
     output = []
 
     for route in route_list:
-        #print(route)
         output.append(route)
         for i in range(1,len(route)):
             output.append(route[:-i])
-        #print(b)
     return output
 
-# delete routes with repeating locs
-# delete routes with repeating locs
-# delete routes with repeating locs
+#######################################################################
+# delete trips with repeating locs
+# default start end location doesn't have to be same
+# possible to force equal start & end location
 def del_repeats(df,routes,bags,start_end_equal = False):
     output = []
     for route in routes:
         temp = []
         df1 = df.loc[df.bags_allowed >= bags,:]
-        # set proper unique identifier here brah
-        #df1 = df1.set_index('flight_number')
 
         # create source location
         temp.append(df1.loc[route[0],:].source)
+
         # append destinations to source etc
-        for item in route:     
+        for item in route:
             temp.append(df1.loc[item,:].destination)
 
         output.append(temp)
 
-    #print(output)
 
-    # only those where all are unique or all unique exept for start & end
-
-    # in case start and end has to be same
+    # only those where all are unique or all unique except for start & end
     if start_end_equal:
         indexer = [counter for counter,i in enumerate(output) if ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
         output = [i for _,i in enumerate(output) if ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
@@ -74,20 +72,22 @@ def del_repeats(df,routes,bags,start_end_equal = False):
         indexer = [counter for counter,i in enumerate(output) if (len(set(i)) == len(i)) or ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
         output = [i for _,i in enumerate(output) if (len(set(i)) == len(i)) or ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
 
-    #print(indexer)
-    #print(output)
     return indexer
 
+############################################################################
 # compute price for trips
 def add_price(df,routes,bags):
     output = []
-    #df = df.set_index('flight_number')
 
     for route in routes:
         price = np.sum(df.loc[route,:].price + df.loc[route,:].bag_price*bags)
         output.append(np.append(route,price))
     return output
 
+
+###########################################################################
+# map indices on rows for postprocessing
+# possible to create loc only + price outputs
 def map_index(df,routes,locs_only=False):
     output = []
 
