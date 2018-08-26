d27f87e45014320af210e947962c46d509df3506
Matus Chladek <mmchladek@gmail.com>
2018-08-26 18:02:28 +0200

    Initial
    First commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+%reload_ext autoreload
+%autoreload 2
+from utils import *
+
+# read input? and parse date/time
+df = pd.read_csv('data1.csv',parse_dates=True)
+df.departure = pd.to_datetime(df.departure,format='%Y-%m-%dT%H:%M:%S')
+df.arrival = pd.to_datetime(df.arrival,format='%Y-%m-%dT%H:%M:%S')
+locs = df.source.unique()
+bags = 1
+output = {}
+
+for start in locs:
+    print(start)
+    flights = []
+
+    # create list of all flights from start destination
+    # consider flight_number to be unique index temporarily
+    df_start = df.loc[df.source == start,:]
+    #print(df_start)
+    df_start = df_start.loc[df_start.bags_allowed >= bags,:]
+    for item in df_start['flight_number']:
+        flights.append([item])
+
+    flights = np.array(flights)
+    #print(flights)
+
+    # longest trips from start destination for each flight
+    new = create_flights(df,flights,bags)
+    #print(new)
+
+    # create subsets of longest trips
+    subsets = get_subsets(new)
+    #print(subsets)
+
+    # delete those with repeating locations
+    indexes = del_repeats(df,subsets,1)
+    uniques = [subsets[i] for i in indexes]
+    uniques_set = set(tuple(x) for x in uniques)
+    uniques = [list(x) for x in uniques_set]
+    uniques
+    #print(uniques)
+
+    # add price to the end of each trip
+    final = add_price(df,uniques,bags)
+
+    output[start] = final
---
+++ utils.py
@@ -0,0 +1,83 @@
+def create_flights(df,flights,bags):
+    output = []
+    count_check = 0
+
+    df1 = df.loc[df.bags_allowed >= bags,:]
+    # set proper unique identifier here brah
+    df1 = df1.set_index('flight_number')
+
+    for counter,route in enumerate(flights):
+        #print(route)
+        pls = df1.loc[route[-1],:]
+        # filter just rows with correct source
+        temp_df = df1.loc[df1.source == pls.destination,:]
+        # filter just time wise available fligths
+        temp_df['wait'] = (temp_df.departure - pls.arrival)/pd.Timedelta('1h')
+        temp_df = temp_df.query('1<=wait<=4')
+
+        if len(temp_df)!=0:
+            #checking if there are any new flights at all
+            count_check += 1
+
+            for item in temp_df.index:
+                new_route = np.append(route,item)
+                output.append(new_route)
+        else:
+            output.append(route)
+    #print(count_check)
+
+    if count_check != 0 :
+        output = create_flights(df,output,bags)
+
+    return output
+
+# create all subsets with initial start
+def get_subsets(route_list):
+    output = []
+
+    for route in route_list:
+        #print(route)
+        output.append(route)
+        for i in range(1,len(route)):
+            output.append(route[:-i])
+        #print(b)
+    return output
+
+# delete routes with repeating locs
+def del_repeats(df,routes,bags):
+    output = []
+    for route in routes:
+
+        temp = []
+        df1 = df.loc[df.bags_allowed >= bags,:]
+        # set proper unique identifier here brah
+        df1 = df1.set_index('flight_number')
+
+        # create source location
+        temp.append(df1.loc[route[0],:].source)
+        # append destinations to source etc
+        for item in route:
+            temp.append(df1.loc[item,:].destination)
+
+        output.append(temp)
+
+    #print(output)
+
+    # only those where all are unique or all unique exept for start & end
+    indexer = [counter for counter,i in enumerate(output) if (len(set(i)) == len(i)) or ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
+    output = [i for _,i in enumerate(output) if (len(set(i)) == len(i)) or ((i[0] == i[-1]) and (len(set(i))+1 == len(i)))]
+
+    #print('skap')
+    #print(indexer)
+    #print(output)
+    return indexer
+
+# compute price for trips
+def add_price(df,routes,bags):
+    output = []
+    df = df.set_index('flight_number')
+
+    for route in routes:
+        price = np.sum(df.loc[route,:].price + df.loc[route,:].bag_price*bags)
+        output.append(np.append(route,price))
+    return output
